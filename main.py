179fc1e8d54cc8f8748c953f549fda38e2542be8
E-Cantor <elicantor1993@gmail.com>
2025-03-11 11:43:38 -0400

    B: Second commit. Limiting fps in main. Creating player sprite and asteroid sprite in circleshape.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
 
 
 def main():
@@ -12,9 +13,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    dt = 0
+    clock = pygame.time.Clock
+
     while True:
+        #Handling events, updating game state, and drawing to screen.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        # Limiting the frame rate to x FPS
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
